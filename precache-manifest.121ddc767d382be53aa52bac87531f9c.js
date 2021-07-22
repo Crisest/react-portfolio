fba296858d9625a9e819073f62b63301d3c6d18a
Yorguin Murillo <ymurillo2@ncstudents.niagaracollege.ca>
2021-07-22 12:26:24 -0400

    Updates

---
+++ precache-manifest.121ddc767d382be53aa52bac87531f9c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4dc6d63a9cd7a200f42079da002d444",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eed3d6c3924096f26a6",
+    "url": "/static/css/2.c34640d7.chunk.css"
+  },
+  {
+    "revision": "12678ccda9797d547c76",
+    "url": "/static/css/main.0bbe8400.chunk.css"
+  },
+  {
+    "revision": "6eed3d6c3924096f26a6",
+    "url": "/static/js/2.13457602.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.13457602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12678ccda9797d547c76",
+    "url": "/static/js/main.df535a78.chunk.js"
+  },
+  {
+    "revision": "166cf5f8ec0cbaf35406",
+    "url": "/static/js/runtime-main.d8445b1d.js"
+  },
+  {
+    "revision": "184a5d7cd8f27ba81033563c3b98386a",
+    "url": "/static/media/004.184a5d7c.png"
+  },
+  {
+    "revision": "bcb511803e572704d0a8213c0e37f877",
+    "url": "/static/media/Squash_Niagara_Logo.bcb51180.png"
+  },
+  {
+    "revision": "3a6851a52b8c380a613be6c4101f107e",
+    "url": "/static/media/YM_july.3a6851a5.pdf"
+  },
+  {
+    "revision": "53d1620198a8019b9f7849900ae9c1b2",
+    "url": "/static/media/code.53d16201.svg"
+  },
+  {
+    "revision": "0a7980e5af03d84b95b219dae8267387",
+    "url": "/static/media/email.0a7980e5.svg"
+  },
+  {
+    "revision": "388dfb45ceff56860d712202a5bb9ff1",
+    "url": "/static/media/expensify.388dfb45.png"
+  },
+  {
+    "revision": "7f452211975889d89f0b166a12d36b75",
+    "url": "/static/media/indApp.7f452211.png"
+  },
+  {
+    "revision": "a94a45623cd5fafe2a35c33c514feae9",
+    "url": "/static/media/phone.a94a4562.svg"
+  },
+  {
+    "revision": "291a2355e695464e0a18186c0da6a595",
+    "url": "/static/media/task-api.291a2355.png"
+  },
+  {
+    "revision": "d3a0a4214815c02a88cb0c1d73e2bf0f",
+    "url": "/static/media/weather-app.d3a0a421.png"
+  }
+]);
